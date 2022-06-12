c8e608eb68a6b955fa459f61d9041b8d8abe0c14
petean09 <petean09@luther.edu>
2022-06-11 19:52:30 -0500

    changing AppComponent title

--- personal-website/src/app/app.component.spec.ts
+++ personal-website/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'personal-website'`, () => {
+  it(`should have as title 'Digital Anna'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('personal-website');
+    expect(app.title).toEqual('Digital Anna');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('personal-website app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Digital Anna app is running!');
   });
 });

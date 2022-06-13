d401968fcb24e2ae7aa51add3165b8cb43b6fa38
petean09 <petean09@luther.edu>
2022-06-12 21:05:49 -0500

    creating navbar code, not working

--- personal-website/src/app/app.component.ts
+++ personal-website/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Digital Anna';
+
+  @ViewChild(AppComponent)
+  links!: AppComponent;
+
+  hamburger = document.querySelector(".hamburger") as HTMLElement;
+  // navLinks = document.querySelector(".nav-links") as HTMLElement;
+  // links = document.querySelectorAll<HTMLElement>(".nav-links li");
+
+  constructor() {}
+
+  navClick() {
+    console.log("here");
+    console.log(this.links);
+    //    //Animate Links
+    //    this.navLinks.classList.toggle("open");
+    //    this.links.forEach(link => {
+    //        link.classList.toggle("fade");
+    //    });
+   
+    //    //Hamburger Animation
+    //    this.hamburger.classList.toggle("toggle");
+  }
+
 }
+
+
